d13141ece473ddcca04faad6f0bf54e7d6e995dd
ccm7676 <50554024+ccm7676@users.noreply.github.com>
2020-12-01 19:23:36 +0000

    Update eula.js

--- src/eula.js
+++ src/eula.js
@@ -15,7 +15,7 @@ module.exports = () => {
   } else {
     fs.writeFileSync(
       'eula.txt',
-      '# By changing this value to true you agree to the Minecraft EULA @ https://account.mojang.com/documents/minecraft_eula.\neula=false\n'
+      '# By changing this value to true you agree to the Minecraft EULA @ https://account.mojang.com/documents/minecraft_eula.\neula=true\n'
     );
     console.error('Please agree to the Minecraft EULA in eula.txt');
     process.exit(1);
